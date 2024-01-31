d46f1ab48d5063dd636c46c3e22e61b37726ec73
STUDIO 17A <88838883+studio17a@users.noreply.github.com>
2024-01-31 07:22:35 +0000

    feat: Remove unnecessary allowed origins and add new allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,20 +1,8 @@
 const allowedOrigins = [
-  "https://ktmjn2-3000.csb.app/dash/users/",
-  "https://ktmjn2-3000.csb.app/dash/users",
-  "https://ktmjn2-3000.csb.app/dash/",
-  "https://ktmjn2-3000.csb.app/dash",
-  "https://ktmjn2-3500.csb.app/dash/users/",
-  "https://ktmjn2-3500.csb.app/dash/users",
-  "https://ktmjn2-3500.csb.app/dash/",
-  "https://ktmjn2-3500.csb.app/dash",
-  "https://ktmjn2-3500.csb.app/",
-  "https://ktmjn2-3000.csb.app/",
-  "https://codesandbox.io",
-  "https://codesandbox.io/p/sandbox/app-base-tg3vhf/",
-  "http://localhost:3000/",
-  "https://localhost:3000/",
-  "https://ktmjn2-3500.csb.app/auth",
-  "https://ktmjn2-3500.csb.app/auth/",
+  "https://warszt.app",
+  "http://warszt.app",
+  "https://www.warszt.app",
+  "http://www.warszt.app",
 ];
 
 module.exports = allowedOrigins;
