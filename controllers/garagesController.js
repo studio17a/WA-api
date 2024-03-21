fae5a8d8d69f3686b9d5c975653993bcabfcd509
marek walczak <walczak.m@icloud.com>
2024-03-21 10:57:20 +0000

    3

--- controllers/garagesController.js
+++ controllers/garagesController.js
@@ -1,17 +1,6 @@
 const Garage = require("../models/Garage");
 
 const getAllGarages = async (req, res) => {
-  res.header(
-    "Access-Control-Expose-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  );
-  res.header("Access-Control-Allow-Origin", "https://tg3vhf-3000.csb.app");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  );
   const garages = await Garage.find().lean();
   if (!garages?.length) {
     return res.status(400).json({ message: "No garages found" });
